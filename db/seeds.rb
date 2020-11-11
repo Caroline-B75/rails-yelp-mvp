7df005dbb348ea3fed64bfcf7e20ed50c69ba3d9
Caroline Belmas <caroline.belmas@gmail.com>
2020-11-11 16:03:11 +0100

    Rails Yelp MVP Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "L'Ardoise du XV", address: "70 Rue Sébastien Mercier, 75015 Paris", phone_number: "06 67 89 67 56", category: "french")
+Restaurant.create(name: "Chef Elias", address: "54 Rue de la Convention, 75015 Paris", phone_number: "01 67 67 87 56", category: "italian")
+Restaurant.create(name: "L'Os à Moelle", address: "3 Rue Vasco de Gama, 75013 Paris", phone_number: "01 67 87 98 45", category: "japanese")
+Restaurant.create(name: "Mademoiselle", address: "89 Avenue Emile Zola, 75008 Paris", phone_number: "01 56 78 46 23", category: "italian")
+Restaurant.create(name: "Le Blavet", address: "62 Rue des Entrepreneurs, 75014 Paris", phone_number: "01 56 21 34 98", category: "french")
+Restaurant.create(name: "La Terrasse", address: "51 Rue du Théâtre, 75015 Paris", phone_number: "01 78 92 83 72", category: "belgian")
+Restaurant.create(name: "Au Rendez-Vous", address: "21 Avenue Félix Faure, 75015 Paris", phone_number: "01 56 21 34 98", category: "french")
+Restaurant.create(name: "L'atypic", address: "32 Rue Gutenberg, 75011 Paris", phone_number: "01 56 21 34 98", category: "french")
+Restaurant.create(name: "Martin Stéphane", address: "126 rue Saint Charles, 75010 Paris", phone_number: "01 56 21 34 98", category: "french")
